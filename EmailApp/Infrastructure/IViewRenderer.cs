ff1b408361805b7b6dd4347d0e20e33002884533
FoxGeoff <fox.geoffrey@gmail.com>
2019-11-05 17:17:12 -0800

    Add: An injectable view renderer

---
+++ EmailApp/Infrastructure/IViewRenderer.cs
@@ -0,0 +1,7 @@
+﻿namespace WebUi.Infrastructure
+{
+    public interface IViewRenderer
+    {
+        string Render<TModel>(string name, TModel model);
+    }
+}
---
+++ EmailApp/Infrastructure/ViewRenderer.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Abstractions;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.AspNetCore.Mvc.Razor;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.ViewEngines;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Routing;
+using System;
+using System.IO;
+
+namespace WebUi.Infrastructure
+{
+    public class ViewRenderer : IViewRenderer
+    {
+        private readonly IServiceProvider _serviceProvider;
+        private readonly ITempDataProvider _tempDataProvider;
+        private readonly IRazorViewEngine _viewEngine;
+
+        public ViewRenderer(
+            IRazorViewEngine viewEngine,
+            ITempDataProvider tempDataProvider,
+            IServiceProvider serviceProvider)
+        {
+            _viewEngine = viewEngine;
+            _tempDataProvider = tempDataProvider;
+            _serviceProvider = serviceProvider;
+        }
+
+        public string Render<TModel>(string name, TModel model)
+        {
+            var actionContext = GetActionContext();
+
+            var viewEngineResult = _viewEngine.FindView(actionContext, name, false);
+
+            if (viewEngineResult.Success == false)
+            {
+                throw new InvalidOperationException(string.Format("Couldn't find view '{0}'", name));
+            }
+
+            var view = viewEngineResult.View;
+
+            using (var output = new StringWriter())
+            {
+                WriteToOutput(model, actionContext, view, output);
+                return output.ToString();
+            }
+        }
+
+        private ActionContext GetActionContext()
+        {
+            var httpContext = new DefaultHttpContext
+            {
+                RequestServices = _serviceProvider
+            };
+            return new ActionContext(httpContext, new RouteData(), new ActionDescriptor());
+        }
+
+        private void WriteToOutput<TModel>(TModel model, ActionContext actionContext, IView view, TextWriter output)
+        {
+            var viewContext = new ViewContext(
+                actionContext,
+                view,
+                new ViewDataDictionary<TModel>(new EmptyModelMetadataProvider(), new ModelStateDictionary())
+                {
+                    Model = model
+                },
+                new TempDataDictionary(
+                    actionContext.HttpContext,
+                    _tempDataProvider),
+                output,
+                new HtmlHelperOptions());
+
+            view.RenderAsync(viewContext).GetAwaiter().GetResult();
+        }
+    }
+}
