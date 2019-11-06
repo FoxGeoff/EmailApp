95f862a484018f72bf093d2e707606133c018b2a
FoxGeoff <fox.geoffrey@gmail.com>
2019-11-05 16:26:54 -0800

    Add: Notification of any exceptions

---
+++ EmailApp/Infrastructure/ExceptionLoggingMiddleware.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using WebUi.Features.Messaging;
+
+namespace WebUi.Infrastructure
+{
+    public class ExceptionLoggingMiddleware
+    {
+        private readonly IHostingEnvironment _env;
+        private readonly IMessageService _messageService;
+        private readonly RequestDelegate _next;
+
+        public ExceptionLoggingMiddleware(RequestDelegate next, IHostingEnvironment env, IMessageService messageService)
+        {
+            _env = env;
+            _messageService = messageService;
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception e)
+            {
+                if (_env.IsDevelopment())
+                {
+                    throw;
+                }
+
+                await _messageService.SendExceptionEmailAsync(e, context);
+                // Redirect the user to whatever the appropriate url for an unhandled exception is for your application
+                context.Response.Redirect("https://www.yourdomain.com/500");
+            }
+        }
+    }
+}
